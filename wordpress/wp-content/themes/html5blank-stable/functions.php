64d5807ca620e43db167fc99044f837a45638aa8
AJ Villane <v7zq@fresnou.org>
2019-04-05 20:38:19 +0000

    end of monday

--- wordpress/wp-content/themes/html5blank-stable/functions.php
+++ wordpress/wp-content/themes/html5blank-stable/functions.php
@@ -72,7 +72,7 @@ function html5blank_nav()
 		'container'       => 'div',
 		'container_class' => 'menu-{menu slug}-container',
 		'container_id'    => '',
-		'menu_class'      => 'menu',
+		'menu_class'      => 'nav nav-pills float-right',
 		'menu_id'         => '',
 		'echo'            => true,
 		'fallback_cb'     => 'wp_page_menu',
--- wordpress/wp-content/themes/html5blank-stable/header.php
+++ wordpress/wp-content/themes/html5blank-stable/header.php
@@ -32,19 +32,15 @@
 			<div class="header clearfix">
 
 					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
+			
 					<!-- /logo -->
 
 					<!-- nav -->
-					<nav class="nav" role="navigation">
+					<nav>
 						<?php html5blank_nav(); ?>
 					</nav>
+					<h1><?php the_title(); ?></h1>
 					<!-- /nav -->
 
-			</header>
+			</div>
 			<!-- /header -->
